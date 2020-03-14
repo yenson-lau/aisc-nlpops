835ec3788a9e30ace142d41b28169a5c3a6f56fa
Yenson Lau <yenson.lau@gmail.com>
2020-03-14 14:53:42 -0400

    Updated datasets and added LexRank notebook.

--- datasets.py
+++ datasets.py
@@ -1,4 +1,4 @@
-import os, tarfile
+import os, re, tarfile
 from easydict import EasyDict as edict
 from random import randint
 from torch.utils.data import Dataset, DataLoader
@@ -11,15 +11,46 @@ def doclen(fpath):
     if l=='':  i -= 1
     return i
 
-def getline(fpath, idx):
+def getlines(fpath, idx, flen=None):
+  # This function will not work if idx is a tuple with duplicates
+
+  # convert into a list of indices
+  t = type(idx)
+  assert t in [int, tuple, slice], 'Index must be an int, tuple, or slice.'
+
+  if t == tuple:  idxs = list(idx)
+
+  if t == int:
+    if idx < 0:
+      assert (flen is not None), \
+        'When using a negative int, file length (flen) must be specified.'
+      idx += flen
+    idxs = [idx]
+
+  if t == slice:
+    assert (flen is not None), \
+      'When using a slice, file length (flen) must be specified.'
+    idxs = list(range(flen))[idx]
+
+  # create a list of outputs
+  out = [None]*len(idxs)
+  rem = idxs.copy()
   with open(fpath, 'r') as f:
     for i, l in enumerate(f):
-      if i == idx:  return l
-    return None
+      if i in rem:            # if line in remaining indices
+        j = idxs.index(i)     # find corresponding index
+        out[j] = l            # update the output
+        # idxs[j] = None      # remove from idxs (in case of duplicates)
+        rem.remove(i)
+      if len(rem)==0: break
+
+  return out[0] if type(idx)==int else out
+
 
 
 class GigaWord(Dataset):
-  def __init__(self, directory='data/sumdata/train', split='train'):
+  def __init__(self, directory='data/sumdata/train',
+               split='train', outFilt=None):
     super(GigaWord, self).__init__()
 
     self.split = split
@@ -28,22 +59,24 @@ class GigaWord(Dataset):
     else:
       self.fpaths = dict(src='valid.article.filter.txt', tgt='valid.title.filter.txt')
     self.fpaths = edict({k: os.path.join(directory, v) for k,v in self.fpaths.items()})
-    self.len = None
+    self.len = doclen(self.fpaths.src)
 
   def __len__(self):
-    if self.len is None:  self.len = doclen(self.fpaths.src)
     return self.len
 
   def __getitem__(self, idx):
-    return edict({k: getline(v,idx) for k,v in self.fpaths.items()})
+    out = edict({k: getlines(v,idx,self.len) for k,v in self.fpaths.items()})
+    return out if (self.outFilt is None) else self.outFilt(out)
+
 
 
 class CNNDailyMail(Dataset):
-  def __init__(self, directory='data/cnndm', split='train'):
+  def __init__(self, directory='data/cnndm', split='train', outFilt=None):
     super(CNNDailyMail, self).__init__()
 
     self.directory = directory
     self.split = split
+    self.outFilt = outFilt
 
     self.fpaths = edict(src=None, tgt=None)
     self.len = None
@@ -63,7 +96,7 @@ class CNNDailyMail(Dataset):
 
         print('Extracting.')
         with tarfile.open(fname, 'r:gz') as tar:
-          tar.extractall(path=directory)
+          tar.extractall(path=self.directory)
 
         print('Done.', end=' ')
 
@@ -83,8 +116,14 @@ class CNNDailyMail(Dataset):
     # Get dataset length
     self.len = doclen(self.fpaths.src)
 
+  def split_tags(self, seg):
+    lines = map(lambda l: re.sub('<t>','',l).strip(), seg.split(r'</t>'))
+    lines = filter(lambda line: len(line)>0, lines)
+    return list(lines)
+
   def __len__(self):
     return self.len
 
   def __getitem__(self, idx):
-    return edict({k: getline(v,idx) for k,v in self.fpaths.items()})
+    out = edict({k: getlines(v,idx,self.len) for k,v in self.fpaths.items()})
+    return out if (self.outFilt is None) else self.outFilt(out)
