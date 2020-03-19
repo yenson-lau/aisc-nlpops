92acd5dbd2d6a196a71c181ac7fe095ada9d55c8
Yenson Lau <yenson.lau@gmail.com>
2020-03-19 00:58:42 -0400

    Started packaging & serving.

--- app.py
+++ app.py
@@ -1,28 +1,47 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, flash, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 import mlflow.pyfunc
 import pandas as pd
 import json
 
+
+class RequestForm(FlaskForm):
+  rss_url = StringField('URL to RSS feed', validators=[DataRequired()])
+  html_tgts = StringField('Target elements', validators=[DataRequired()])
+  submit = SubmitField('Summarize')
+
 # Name of the apps module package
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'any secret string'
 
 # Load in the model at app startup
-model = mlflow.pyfunc.load_model('./model')
+# model = mlflow.pyfunc.load_model('./model')
 
 # Load in our meta_data
 f = open("./model/code/meta_data.txt", "r")
 load_meta_data = json.loads(f.read())
 
 # Meta data endpoint
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def meta_data():
   return jsonify(load_meta_data)
 
-@app.route('/rss/<rss>', methods=['GET'])
-def summarize_rss(rss):
-  out = [f'URL: {rss}']
-  out.append(f'Length: {len(rss)}')
+def get_rss_summary(rss_url, html_tgts):
+  out = [rss_url, html_tgts]
   return '<br/>'.join(out)
+
+@app.route('/rss', methods=['GET', 'POST'])
+def req_rss_sum():
+  form = RequestForm()
+
+  if form.validate_on_submit():
+    flash(f'Summary requested for URL {form.rss_url.data}...')
+    return get_rss_summary(form.rss_url.data, form.html_tgts.data)
+
+  return render_template('rss-summarize.html',
+                         title='Summarize', form=form)
 
 # Prediction endpoint
 @app.route('/predict', methods=['POST'])
